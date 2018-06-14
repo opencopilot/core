460183422598f9598496308c9a58f22f42e2f230
Patrick DeVivo <patrick.devivo@gmail.com>
2018-06-14 11:57:29 -0400

    debugging

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"log"
 
 	consul "github.com/hashicorp/consul/api"
 	vault "github.com/hashicorp/vault/api"
@@ -231,7 +232,9 @@ func (s *server) GetApplication(ctx context.Context, in *pb.GetApplicationReques
 		return nil, errors.New("Invalid auth provider")
 	}
 
+	log.Println(in.ApplicationId)
 	app, err := GetPacketApplication(s.consulClient, in.ApplicationId)
+	log.Println(app)
 	if err != nil {
 		return nil, err
 	}
